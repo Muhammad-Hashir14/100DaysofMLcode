4f5723861c50749b965e76958391f7c9a1ba59ac
Muhammad Hashir <hashirgohar@gmail.com>
2023-03-20 15:55:59 +0500

    Added code of operators and expressions

---
+++ Python/operators and expressions.py
@@ -0,0 +1,26 @@
+print("Operators and Expressions")
+# Taking input
+print("Enter Your name")
+a = input()
+print(a)
+
+num1 = 45
+num2 = 12
+# Addition
+print(num1 + num2)
+# Substraction
+print(num1 - num2)
+# Multiplicaion
+print(num1 * num2)
+# Division
+print(num1 / num2)
+print(num1 // num2)
+# power
+print(num1 ** num2)
+# Comparasion
+print(num1 == num2)
+print(num1 != num2)
+
+num1 = int(input("Enter number 1"))
+num2 = int(input("Enter number 2"))
+print(num1 + num2)
