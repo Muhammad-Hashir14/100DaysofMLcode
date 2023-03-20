eaa99b3bf97b81281b98cb20694e071a4640fd51
Muhammad Hashir <hashirgohar@gmail.com>
2023-03-20 15:20:43 +0500

    Just added python variables code

---
+++ Python/variables in python.py
@@ -0,0 +1,12 @@
+students_counts = 1000
+rating = 4.99
+is_published = False
+course_name = "python Programming"
+print(type(students_counts))
+print(type(rating))
+print(type(is_published))
+print(type(course_name))
+print(students_counts)
+print(rating)
+print(is_published)
+print(course_name)
